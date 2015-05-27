cdaebcb785e4c24df0a5f1d20a65a960ef201698
Fabio N. Kepler <fabio@kepler.pro.br>
2015-05-27 11:55:09 -0300

    Add missing dependency on underscore.string.

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'kepler:autoform-wizard',
   summary: 'A multi step form component for AutoForm.',
-  version: '0.0.2',
+  version: '0.0.2_1',
   git: 'https://github.com/kepler/meteor-autoform-wizard.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  
+
   api.use([
     'underscore',
     'tracker',
@@ -18,16 +18,17 @@ Package.onUse(function(api) {
     'ejson',
     'localstorage'
   ], 'client');
-  
+
   api.use('aldeed:autoform@5.0.0', 'client');
-  
+  api.use('underscorestring:underscore.string@3.0.3', ['client', 'server']);
+
   api.addFiles([
     'wizard.html',
     'wizard.js',
     'router.js',
     'cache.js'
   ], 'client');
-  
+
   if (api.export)
     api.export('Wizard', 'client');
 });
